937dfcfca67fd3c745968855db554265d77614d0
ehsanrs2 <ehsan.r.s2@gmail.com>
2023-02-06 23:21:01 +0330

    add:Dashboard demo file

---
+++ dashboard/Interface_of_Dashboard.py
@@ -0,0 +1,26 @@
+import pandas as pd 
+from sqlalchemy.engine import URL
+import sqlalchemy as db
+
+
+url_object = URL.create(
+    "mysql+mysqlconnector",
+    username="root",
+    password="ehsan",
+    host="localhost",    
+    database="Doffeeshopdata",
+)
+
+engine = db.create_engine(url_object)
+conn = engine.connect()
+
+d = pd.DataFrame(engine.execute(
+"""
+SELECT * FROM address Inner join coffeeshop on address.CoffeID = coffeeshop.Id 
+Inner join featuers on featuers.CoffeID = coffeeshop.Id 
+Inner join points on points.CoffeID = coffeeshop.Id 
+""").fetchall())
+d=d.drop([d.columns[4],d.columns[10],d.columns[21]],axis=1) # drop columns of Id of Coffee shops sleeted in Inner join
+id = d.index
+d.insert(0,"Id",id)
+d.to_csv("dash_data.csv",index=False)
